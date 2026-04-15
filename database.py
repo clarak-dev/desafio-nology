66b330473bd4191333058303101d1b4354353f2e
Clara Kricia <clarak.mkt@gmail.com>
2026-04-14 23:14:15 -0300

    chore: prepara deploy no render

--- database.py
+++ database.py
@@ -6,14 +6,18 @@ load_dotenv()
 
 
 def conectar():
-    conexao = psycopg2.connect(
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    return psycopg2.connect(
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
-    return conexao
 
 
 def criar_tabela():
